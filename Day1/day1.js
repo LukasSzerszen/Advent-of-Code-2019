4472a34770ddb05ba0f6d2de615176d4e90827a4
Lukas <lukassz@kth.se>
2019-12-02 16:30:36 +0100

    fix: lint

--- Day1/day1.js
+++ Day1/day1.js
@@ -5,14 +5,13 @@ const readInterface = (output, path) => {
     return readline.createInterface({
         input: fs.createReadStream(path),
         output: output,
-        console:false
+        console: false
     })
 }
 
-
 const star1 = () => {
     let sum = 0
-    const rl = readInterface(sum,'Inputs/Day1/star1.txt')
+    const rl = readInterface(sum, 'Inputs/Day1/star1.txt')
     rl.on('line', (line) => {
         sum += calcFuel(line)
 
@@ -26,9 +25,9 @@ const star1 = () => {
 
 const star2 = () => {
     let sum = 0
-    const rl = readInterface(sum,'Inputs/Day1/star1.txt')
+    const rl = readInterface(sum, 'Inputs/Day1/star1.txt')
     rl.on('line', (line) => {
-        sum += calculateAllFuel(0,line)
+        sum += calculateAllFuel(0, line)
     })
 
     rl.on('close', () => {
@@ -41,9 +40,8 @@ const calcFuel = (fuel) => {
     return Math.floor(fuel / 3) - 2
 }
 
-
-const calculateAllFuel = (total, fuel) =>{
-    return (subModuleFuel = calcFuel(fuel)) > 0 ? calculateAllFuel(total + subModuleFuel,subModuleFuel ) : total 
+const calculateAllFuel = (total, fuel) => {
+    return (subModuleFuel = calcFuel(fuel)) > 0 ? calculateAllFuel(total + subModuleFuel, subModuleFuel) : total
 }
 
 star1()
